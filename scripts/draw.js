c48f6d36260f22ffed1d8db306cfcc17405ae86e
Igor <igor.bobeldijk@gmail.com>
2017-01-06 14:17:32 +0100

    instant drawing

--- scripts/draw.js
+++ scripts/draw.js
@@ -9,6 +9,7 @@ window.onload = function() {
 	var bg = new Path.Rectangle(view.bounds);
 	bg.fillColor = "#787b7a";
 	project.activeLayer.clipped = true;
+	view.draw();
 	
 	socket.on('imageRequest', function() {
 		var img = project.activeLayer.rasterize().toDataURL();
